b3022c4be482f19df032a7a41b1a6ab3f4c6e30b
chuksmili <chukwudimmiliogu0@gmail.com>
2024-01-04 19:25:27 -0800

    cyclical list linking

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
   * check_cycle - function that check if list is cyclical
@@ -6,7 +6,7 @@
   * Return: 1 if there is cycle and 0 if not
   */
 
-int check_cycle(listint_t *list);
+int check_cycle(listint_t *list)
 {
 	listint_t *fast, *slow;
 	if (!list || !list->next)
@@ -14,7 +14,7 @@ int check_cycle(listint_t *list);
 	fast = list;
 	slow = list;
 	
-	while (slow !- NULL && fast != NULL && fast->next != NULL)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
